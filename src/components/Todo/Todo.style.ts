a49b91597873957eb5415b9f9ba200d2ed29dfe0
whyareyou <bionixxxd5@gmail.com>
2023-06-10 23:06:34 +0300

    add icon for todo button

--- src/components/Todo/Todo.style.ts
+++ src/components/Todo/Todo.style.ts
@@ -1,32 +1,50 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 type TodoTextProps = {
-	completed: boolean;
+    completed: boolean;
 };
 
 export const TodoWrapper = styled.div`
-	display: flex;
-	align-items: center;
-	margin-bottom: 8px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
 `;
 
 export const TodoButton = styled.button<TodoTextProps>`
-	margin-right: 8px;
-	appearance: none;
-	width: 20px;
-	height: 20px;
-	border: 2px solid #cfb2c8;
-	border-radius: 50%;
-	cursor: pointer;
-	outline: none;
-	background-color: transparent;
+  margin-right: 8px;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border: 2px solid #cfb2c8;
+  border-radius: 25%;
+  cursor: pointer;
+  outline: none;
+  background-color: transparent;
+  position: relative;
+
+  ${props => props.completed && css`
+    &::before {
+      content: '✓';
+
+      height: 15px;
+      width: 15px;
+      display: block;
+
+      position: absolute;
+      top: -6px;
+      left: 2px;
+
+      font-size: 24px;
+      color: #3dc938;
+    }
+  `}
 `;
 
 export const TodoText = styled.span<TodoTextProps>`
-	flex: 1;
-	font-size: 16px;
-	color: #de9ecf;
+  flex: 1;
+  font-size: 16px;
+  color: #de9ecf;
 
-	${(props) => props.completed && "text-decoration: line-through;"};
-	word-break: break-all;
+  ${(props) => props.completed && "text-decoration: line-through;"};
+  word-break: break-all;
 `;
